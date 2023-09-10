f3f479268a3943c3dfc9e377bec6595e7be20d86
phonixsetup <sr8020sr@gmail.com>
2023-09-11 00:17:00 +0330

    all done work good
    fix some bugs

--- banattacker.py
+++ banattacker.py
@@ -3,7 +3,7 @@ import re
 
 
 def get_online_users(port):
-    list_cmd = f"sudo lsof -i:{port} -n | grep -v root | grep ESTABLISHED"
+    list_cmd = f"sudo lsof -i:{port} -n |  grep ESTABLISHED"
     try:
         list_output = subprocess.check_output(
             list_cmd, shell=True, stderr=subprocess.DEVNULL).decode("utf-8")
@@ -27,15 +27,25 @@ def get_online_info(port):
             if len(matches) == 2:
                 ipz = matches[1]
                 atacker_count[ipz] = atacker_count.get(ipz, 0) + 1
-    print (atacker_count)
+    print(atacker_count)
     return atacker_count
+
 
 def ban_ip(ip):
     banip_command = "sudo ufw insert 1 deny from {}".format(ip)
-    reload_command = "sudo ufw --force reload"
 
     try:
         subprocess.run(banip_command, shell=True, check=True)
+        return True
+    except subprocess.CalledProcessError as e:
+        print(e)
+        return False
+
+
+def relood_ufw():
+    reload_command = "sudo ufw --force reload"
+
+    try:
         subprocess.run(reload_command, shell=True, check=True)
         return True
     except subprocess.CalledProcessError as e:
@@ -43,10 +53,12 @@ def ban_ip(ip):
         return False
 
 
-
 badip_count = get_online_info(443)
 if badip_count != None:
     for ip in badip_count:
-        if badip_count[ip] > 3 :
+        if badip_count[ip] > 10:
             result = ban_ip(ip)
-            print (ip)
+            print(ip)
+
+relood_ufw(ip)
+print("done")
